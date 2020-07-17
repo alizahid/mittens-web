1ad917824767f3fcd7c506bf5daf9fbbcc692521
Ali Zahid <ali.zahid@live.com>
2020-07-17 09:32:20 +0400

    Styles updated

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,7 @@ const Home: NextPage = () => (
       <title>mittens brings you push notifications from GitHub</title>
     </Head>
 
-    <header className="flex flex-col items-center justify-center my-12">
+    <header className="flex flex-col items-center justify-center my-20">
       <img alt="mittens" className="h-20" src="/mittens.svg" />
       <h1 className="text-secondary font-medium text-3xl mt-4 mb-2">mittens</h1>
       <p className="text-center">
@@ -19,7 +19,7 @@ const Home: NextPage = () => (
     </header>
 
     <main>
-      <section className="flex justify-center flex-wrap -mt-4">
+      <section className="flex justify-center flex-wrap -mt-16">
         <a
           className="bg-secondary text-white font-medium rounded-full px-4 py-3 m-4"
           href="https://itunes.apple.com/us/app/mittens/id1453383568?mt=8">
@@ -36,7 +36,7 @@ const Home: NextPage = () => (
           GitHub
         </a>
       </section>
-      <section className="mt-12 flex flex-col lg:flex-row justify-center">
+      <section className="mt-20 flex flex-col lg:flex-row justify-center">
         <figure className="rounded-lg overflow-hidden">
           <img
             alt="Mittens light"
@@ -54,7 +54,7 @@ const Home: NextPage = () => (
       </section>
     </main>
 
-    <footer className="flex items-center justify-center my-12 text-gray-500">
+    <footer className="flex items-center justify-center my-20 text-gray-500">
       made with
       <img alt="love" className="h-6 w-6 mx-1" src="/heart.svg" />
       by
